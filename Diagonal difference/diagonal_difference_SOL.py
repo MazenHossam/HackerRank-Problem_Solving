03c05b349643d40118f9517007156494ae768680
MazenHossam <60198948+MazenHossam@users.noreply.github.com>
2022-03-21 00:25:47 +0200

    Create diagonal_difference_SOL.py

---
+++ Diagonal difference/diagonal_difference_SOL.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    sums=[0,0]
+    
+    for i in range(len(arr[0])):
+        sums[0]+=arr[i][i]
+        sums[1]+=arr[i][len(arr[0])-1-i]
+    return abs(sums[0]-sums[1])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
