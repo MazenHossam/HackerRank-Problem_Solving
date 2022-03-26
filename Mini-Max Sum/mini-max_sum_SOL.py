5ccfa7fe9f3f84ddc0acf1b774db06c74b488ba5
MazenHossam <60198948+MazenHossam@users.noreply.github.com>
2022-03-27 00:49:53 +0200

    Create mini-max_sum_SOL.py

---
+++ Mini-Max Sum/mini-max_sum_SOL.py
@@ -0,0 +1,15 @@
+import math
+import os
+import random
+import re
+import sys
+
+def miniMaxSum(arr):
+    arr.sort()
+    print("{} {}".format(sum(arr[:-1]),sum(arr[1:])))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
