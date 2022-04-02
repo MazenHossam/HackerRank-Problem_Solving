f2d765648c7be7f662a0a3a74fa9c838a4896d44
MazenHossam <60198948+MazenHossam@users.noreply.github.com>
2022-04-02 23:43:48 +0200

    Update grading_students_SOL.py

--- Grading students/grading_students_SOL.py
+++ Grading students/grading_students_SOL.py
@@ -1,19 +1,8 @@
-
-
-
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'gradingStudents' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY grades as parameter.
-#
 
 def gradingStudents(grades):
     final_grades=grades
